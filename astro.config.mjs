15b87a9c4289af2bdfff4ef3cd8cb1f2cc53d4c2
AvaroDev <75514102+alvarosp1@users.noreply.github.com>
2025-01-24 13:13:48 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://alvarosp1.github.io',
+  site: 'https://asistentesdeia.cl',
   base: '/AsistentesDeIA',
-});
+}); 
